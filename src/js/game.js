d5dcb62e6d194b48e36504ad6da2be3b24aebf79
racingcow <racingcow@hotmail.com>
2015-07-06 23:41:42 -0500

    bit better timing checks

--- src/js/game.js
+++ src/js/game.js
@@ -1,21 +1,68 @@
-(function() {
+(function(_) {
   
   'use strict';
     
-  var cursorKeys,
-      startTime, 
+  var startTime, 
       timeText,
       nextText,
       balloon,
       leftMargin = 30,
       bottomRow = 375,
-      tolerance = 2000,
+      textOpts = {
+        font: '24px Arial',
+        fill: '#ffffff'
+      },
+      ratings = [{
+        name: 'bad',
+        points: 0,
+        tolerance: 600000
+      },{
+        name: 'meh',
+        points: 1,
+        tolerance: 5000
+      },{
+        name: 'good',
+        points: 3,
+        tolerance: 3000
+      },{
+        name: 'great',
+        points: 4,
+        tolerance: 2000
+      },{
+        name: 'perfect',
+        points: 5,
+        tolerance: 1000
+      }],
       songMap = [{
+        time: 5000,
+        keyCode: Phaser.Keyboard.UP
+      },{
         time: 10000,
-        keyCode: Phaser.Keyboard.LEFT
+        keyCode: Phaser.Keyboard.DOWN
+      },{
+        time: 15000,
+        keyCode: Phaser.Keyboard.UP
       },{
         time: 20000,
+        keyCode: Phaser.Keyboard.DOWN
+      },{
+        time: 25000,
+        keyCode: Phaser.Keyboard.LEFT
+      },{
+        time: 30000,
         keyCode: Phaser.Keyboard.RIGHT
+      },{
+        time: 35000,
+        keyCode: Phaser.Keyboard.LEFT
+      },{
+        time: 40000,
+        keyCode: Phaser.Keyboard.RIGHT
+      },{
+        time: 45000,
+        keyCode: Phaser.Keyboard.B
+      },{
+        time: 50000,
+        keyCode: Phaser.Keyboard.A
       }];
   
   function Game() {}
@@ -51,23 +98,11 @@
       this.bindKey(Phaser.Keyboard.UP);
       this.bindKey(Phaser.Keyboard.DOWN);
       
-      this.add.text(leftMargin + 125, bottomRow + 40, 'time:', {
-        font: '36px Arial',
-        fill: '#ffffff'
-      });
-      timeText = this.add.text(leftMargin + 225, bottomRow + 40, '0', {
-        font: '36px Arial',
-        fill: '#ffffff'
-      });
+      this.add.text(leftMargin + 125, bottomRow + 40, 'time:', textOpts);
+      timeText = this.add.text(leftMargin + 185, bottomRow + 40, '0', textOpts);
       
-      this.add.text(leftMargin + 375, bottomRow + 40, 'next:', {
-        font: '36px Arial',
-        fill: '#ffffff'
-      });
-      nextText = this.add.text(leftMargin + 465, bottomRow + 40, '0', {
-        font: '36px Arial',
-        fill: '#ffffff'
-      });
+      this.add.text(leftMargin + 340, bottomRow + 40, 'next:', textOpts);
+      nextText = this.add.text(leftMargin + 400, bottomRow + 40, '0', textOpts);
       
     },
     
@@ -77,32 +112,56 @@
     },
 
     update: function () {
+      
       timeText.setText(this.time.now.toString());
-      nextText.setText(songMap.length ? songMap[0].time.toString() : '');
+      
+      var elapsed = this.time.now - startTime;
+      var next = _.chain(songMap)
+       .filter(function(mapItem) {
+         return mapItem.time > elapsed;
+       })
+       .first()
+       .value();
+
+      nextText.setText(next && next.time ? next.keyCode.toString() + ' @ ' + next.time.toString() : '');
+
     },
     
     keyHandler: function(data) {
       
-      if (!songMap.length) return;
-      var elapsed = this.time.now - startTime;      
-      var next = songMap.shift();
+      var elapsed = this.time.now - startTime;
       
-      if (data.keyCode === next.keyCode) {
-        console.log('you pressed the correct key!');
-      }
-      else {
-        console.log('wrong key (expected ' + next.keyCode + ')');
+      var map = songMap;
+      var result = _.chain(ratings)
+        .map(function(rating) {
+        
+          map = _.filter(map, function(mapItem) { //a little faster, perhaps
+          
+           var halfTolerance = rating.tolerance / 2,
+               low = elapsed - halfTolerance,
+               high = elapsed + halfTolerance;
+          
+           return (mapItem.time >= low) && (mapItem.time <= high) && (mapItem.keyCode === data.keyCode);
+          });
+          
+          rating.hits = map;
+          return rating;
+        })
+        .filter(function(rating) {
+          return rating.hits && rating.hits.length;
+        })
+        .sortBy('tolerance')
+        .first()
+        .value();
+        
+      if (result) {
+        console.log(result.name);
       }
       
-      if ((elapsed <= (next.time + (tolerance / 2))) && (elapsed >= (next.time - (tolerance / 2)))) {
-          console.log('your timing is great');
-      }
-      else {
-        console.log('your timing is off - elapsed = ' + elapsed.toString() + ', expected = ' + next.time.toString());
-      }
     }
-  };
+
+  }; 
 
   window['dance-monkey-dance'] = window['dance-monkey-dance'] || {};
   window['dance-monkey-dance'].Game = Game;
-}());
+}(_));
